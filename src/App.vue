907e69c67e61d752d59363a594b3648acba3893c
Khanh Nguyen <kqn1995@gmail.com>
2019-10-12 14:48:20 -0400

    feat(login): create login page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+});
+</script>
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .flex-container {
+    display: flex;
+    flex-direction: column;
   }
-}
+  .flex-container-row {
+    display: flex;
+    flex-direction: row;
+  }
+  .flex-item-variable {
+    flex: 1 1 auto;
+  }
 </style>
---
+++ src/views/login/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex-container flex-item-variable center-align">
+    <img alt="Smart Cabinet logo" src="../../assets/smart-cabinet-logo.png">
+    <v-form>
+      <v-text-field
+        label="Username"
+        name="username"
+        prepend-icon="person"
+        type="text"
+      ></v-text-field>
+      <v-text-field
+        id="password"
+        label="Password"
+        name="password"
+        prepend-icon="lock"
+        type="password"
+      ></v-text-field>
+      <div class="flex-container-row login-button-bar">
+        <v-btn color="primary">Login</v-btn>
+      </div>
+    </v-form>
+  </div>
+</template>
+
+<script lang="ts" src="./login.vue.ts"></script>
+<style lang="scss" scoped>
+  .logo {
+    align-items: center;
+  }
+  .login-button-bar {
+    justify-content: flex-end;
+  }
+  .center-align {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
